cf5115068b6841665b526407118c03fa5e46ff60
MediaBooster1 <info@mediabooster.jp>
2026-02-17 20:31:21 +0900

    Create post_x.py

---
+++ src/post_x.py
@@ -0,0 +1,23 @@
+import os
+import requests
+from requests_oauthlib import OAuth1
+
+API_KEY = os.getenv("X_API_KEY")
+API_SECRET = os.getenv("X_API_SECRET")
+ACCESS_TOKEN = os.getenv("X_ACCESS_TOKEN")
+ACCESS_SECRET = os.getenv("X_ACCESS_TOKEN_SECRET")
+
+url = "https://api.twitter.com/2/tweets"
+
+auth = OAuth1(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
+
+tweet_text = "自動投稿テスト成功 🚀"
+
+payload = {
+    "text": tweet_text
+}
+
+response = requests.post(url, auth=auth, json=payload)
+
+print(response.status_code)
+print(response.text)
